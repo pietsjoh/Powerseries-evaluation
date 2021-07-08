af61690778a01721ad522f24066ffb42b8105a44
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-07-08 10:01:55 +0200

    Add type hints and Raises section

--- pysrc/utils/random_number_gen.py
+++ pysrc/utils/random_number_gen.py
@@ -3,19 +3,23 @@ Can create integers, uniform distributed and normal distributed floats.
 """
 import numpy.random as random
 import numpy as np
+import typing
 
-maxInt32 = 2**31 - 1
-"""largest signed 32-bit representible integer
+intOrNone = typing.Union[int, None]
+number = typing.Union[float, int]
+
+maxInt32: int = 2**31 - 1
+"""int: largest signed 32-bit representible integer
 """
 
-eps = np.finfo(float).eps
-"""precision of float
+eps: float = np.finfo(float).eps
+"""float: precision of float
 """
 
 
 
 class RandomState:
-    def __init__(self, seed=None, printSeed=True):
+    def __init__(self, seed: intOrNone=None, printSeed: bool=True) -> None:
         """A pseudo-random number generator.
 
         Can be used to generate reliably reproducible random number sets.
@@ -26,6 +30,11 @@ class RandomState:
             The generators random number seed. Is chosen at random out of the uniform distribution over [0,`maxInt32`) if not specified ('None').
         printSeed : bool, default = True
             Determines whether to print random seed. Useful for debugging purposes.
+
+        Raises
+        ------
+        AssertionError:
+            When seed or printSeed are invalid datatypes, or when seed is out of range [0, maxInt32] 
         """
         if seed == None:
             seed = random.randint(low=0, high=maxInt32)
@@ -37,10 +46,10 @@ class RandomState:
         if printSeed:
             print("random seed:{}".format(seed) )
 
-        self.seed   = seed
+        self.seed: int = seed
         self.gen = random.default_rng( seed )
 
-    def integers(self, low=0, high=1, size=1):
+    def integers(self, low: int=0, high: int=1, size: int=1) -> np.ndarray[int]:
         """Generates an 'np.ndarray' of 'size' integers.
         The integers lie in the range ['low', 'high').
 
@@ -52,6 +61,16 @@ class RandomState:
             Higher bound of the random number to be generated. Has to be higher than low.
         size: int, default = 1
             Number of integers that are generated.
+
+        Returns
+        -------
+        np.ndarray:
+            Array of "size" uniform distributed ints in the range of [low, high).
+
+        Raises
+        ------
+        AssertionError:
+            When low, high or size are invalid datatypes, or when size is below 1, or when low >= high
         """
         assert np.issubdtype(type(low), np.integer)
         assert np.issubdtype(type(high), np.integer)
@@ -61,7 +80,7 @@ class RandomState:
 
         return self.gen.integers(low=low, high=high, size=size)
 
-    def uniform(self, low=0, high=1, size=1):
+    def uniform(self, low: number=0, high: number=1, size: int=1) -> np.ndarray[float]:
         """Generates an 'np.ndarray' of 'size' floats.
         The floats lie in the range ['low', 'high').
         The floats are uniformly distributed.
@@ -74,6 +93,16 @@ class RandomState:
             Higher bound of the random number to be generated. Has to be higher than low.
         size: int, default = 1
             Number of floats that are generated.
+
+        Returns
+        -------
+        np.ndarray:
+            Array of "size" uniform distributed floats in the range of [low, high)
+
+        Raises
+        ------
+        AssertionError:
+            When low, high or size are invalid datatypes, or when size is below, or when low >= high
         """
         assert np.issubdtype(type(low), np.integer) or np.issubdtype(type(low), np.floating)
         assert np.issubdtype(type(high), np.integer) or np.issubdtype(type(high), np.floating)
@@ -83,7 +112,7 @@ class RandomState:
 
         return (high - low)*self.gen.random(size) + low
 
-    def normal(self, mu=0, sigma=1, size=1):
+    def normal(self, mu: number=0, sigma: number=1, size: int=1) -> np.ndarray[float]:
         """Generates an 'np.ndarray' of 'size' floats.
         The floats are distributed according to the normal distribution N(mu, sigma).
 
@@ -95,6 +124,16 @@ class RandomState:
             Standard deviation of the underlying normal distribution. Has to be greater than machine precision.
         size: int, default = 1
             Number of floats that are generated.
+
+        Returns
+        -------
+        np.ndarray:
+            Array of "size" normal distributed floats
+
+        Raises
+        ------
+        AssertionError:
+            When mu, sigma or size are invalid datatypes, or when size is below 1 or when sigma <= eps (float precision)
         """
         assert np.issubdtype(type(mu), np.integer) or np.issubdtype(type(mu), np.floating)
         assert np.issubdtype(type(sigma), np.integer) or np.issubdtype(type(sigma), np.floating)
