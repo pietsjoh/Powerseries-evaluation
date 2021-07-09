2223bbe975ce7077be67f5ac996dedb23c084fc9
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-07-09 11:00:40 +0200

    Dump stats into file
    instead of showing it in console

--- tests/performance_tests/test_combine_ps_tool.py
+++ tests/performance_tests/test_combine_ps_tool.py
@@ -3,15 +3,17 @@ import pstats
 
 import sys
 from pathlib import Path
-headDir = Path(__file__).parents[2].resolve()
+headDir: Path = Path(__file__).parents[2].resolve()
 sys.path.append(str(headDir))
 
-from pysrc.powerseries.combine_ps_tool import CombinePowerSeriesTool
+performanceProfileFileLocation: str = str((headDir / "tests" / "performance_tests" / "performance_profile.prof").resolve())
 
 with cProfile.Profile() as pr:
+    from pysrc.powerseries.combine_ps_tool import CombinePowerSeriesTool
     test = CombinePowerSeriesTool()
-    test.run()
+    # test.run()
 
 stats = pstats.Stats(pr)
 stats.sort_stats(pstats.SortKey.TIME)
-stats.print_stats()
+# stats.print_stats()
+stats.dump_stats(filename=performanceProfileFileLocation)
