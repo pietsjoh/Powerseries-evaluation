5c052a2638c16f9efcd6925ef3bb97ab2b1366d8
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-07-09 09:46:17 +0200

    Add functions to include __init__ method in docs

--- pysrc/setup/create_doc_files.py
+++ pysrc/setup/create_doc_files.py
@@ -63,7 +63,17 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # favicon (image in the tab or bookmarks)
-html_favicon = str((headDirPath / "res" / 'jp_logo_schnitt.jpg').resolve())"""
+html_favicon = str((headDirPath / "res" / 'jp_logo_schnitt.jpg').resolve())
+
+# include __init__ methods in documentation
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+"""
 
     ## Writing / creating conf.py file
     confpyPath: str = str((docsSourcePath / "conf.py").resolve())
