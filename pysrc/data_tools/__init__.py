aa0e7005360ec812effa0918865f7dd25c69297c
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-07-23 16:04:55 +0200

    Update docstring

--- pysrc/data_tools/__init__.py
+++ pysrc/data_tools/__init__.py
@@ -1,4 +1,3 @@
-"""Contains the base file data_import, which loads the data.
-Later this will be maybe split into 1 base class, and inherit 2 different lab setups (qlab2, qlab5).
+"""Contains the file data_formats, which contains different classes that load the measurement data based on their format.
 sort_data sorts the files of the data directory by diameter and by used grating (fine, non-fine) into the sorted_data directory.
 """
