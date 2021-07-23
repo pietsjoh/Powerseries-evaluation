f59b2150a403ed6a9c5a2f4c24e47db9bad77d16
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-07-23 12:12:25 +0200

    Add type hints

--- pysrc/data_tools/data_formats.py
+++ pysrc/data_tools/data_formats.py
@@ -1,60 +1,63 @@
-import pandas as pd
+import pandas as pd # type: ignore
 import numpy as np
 import sys
-import pathlib
+import typing
+from pathlib import Path
 
-headDirPath = pathlib.Path(__file__).resolve().parents[1]
+headDirPath = Path(__file__).resolve().parents[1]
 sys.path.append(str(headDirPath))
 
 from setup.config_logging import LoggingConfig
 from utils.filename_analysis import FileNameReader
 
-loggerObj = LoggingConfig()
+loggerObj: LoggingConfig = LoggingConfig()
 logger = loggerObj.init_logger(__name__)
 
-class DataQlab2:
-    rowsToSkip = [1, 3]
-    name = "QLAB2"
+number = typing.Union[int, float, np.number]
 
-    def __init__(self, dataPath):
-        assert isinstance(dataPath, pathlib.PurePath)
+class DataQlab2:
+    rowsToSkip: list[int] = [1, 3]
+    name: str = "QLAB2"
+
+    def __init__(self, dataPath: Path) -> None:
+        assert isinstance(dataPath, Path)
         assert dataPath.exists()
         try:
-            self.data = pd.read_csv(dataPath, sep="\t", skiprows=lambda x: x in self.rowsToSkip)
+            self.data: pd.DataFrame = pd.read_csv(dataPath, sep="\t", skiprows=lambda x: x in self.rowsToSkip)
         except:
             logger.exception(f"Data from {str(dataPath)} could not be read.")
             raise ValueError("Data from file could not be read.")
         else:
-            self.wavelengths = self.data["Wavelength"].to_numpy()[1:]
-            self.energies = self.data["Energy"].to_numpy()[1:][::-1]
+            self.wavelengths: np.ndarray = self.data["Wavelength"].to_numpy()[1:]
+            self.energies: np.ndarray = self.data["Energy"].to_numpy()[1:][::-1]
             self.data.drop(["Energy", "Wavelength"], axis=1, inplace=True)
-            self.inputPower = self.data.loc[0].to_numpy()*9
-            self.lenInputPower = self.inputPower.size
+            self.inputPower: np.ndarray = self.data.loc[0].to_numpy()*4
+            self.lenInputPower: int = self.inputPower.size
             self.data.drop([0], axis=0, inplace=True)
-            self.columns = self.data.columns
-            self.strFilePath = str(dataPath)
-            self.maxEnergy = np.amax(self.energies)
-            self.minEnergy = np.amin(self.energies)
-            self.minInputPower = np.amin(self.inputPower)
-            self.maxInputPower = np.amax(self.inputPower)
-            self.fileName = dataPath.name
+            self.columns: list = self.data.columns
+            self.strFilePath: str = str(dataPath)
+            self.maxEnergy: number = np.amax(self.energies)
+            self.minEnergy: number = np.amin(self.energies)
+            self.minInputPower: number = np.amin(self.inputPower)
+            self.maxInputPower: number = np.amax(self.inputPower)
+            self.fileName: str = dataPath.name
             assert len(self.data.columns) == self.lenInputPower, f"{len(self.data.columns)}, {self.lenInputPower}"
             logger.info(f"Data from {self.fileName} successfully loaded.")
 
-    def __getitem__(self, number):
-        return self.data[self.columns[number]].to_numpy()[::-1]
+    def __getitem__(self, idx: int) -> number:
+        return self.data[self.columns[idx]].to_numpy()[::-1]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.lenInputPower
 
     @property
-    def diameter(self):
-        DiameterReader = FileNameReader("diameter", "µm", "_", indicatorAtStart=False)
+    def diameter(self) -> str:
+        DiameterReader: FileNameReader = FileNameReader("diameter", "µm", "_", indicatorAtStart=False)
         return DiameterReader(self.fileName)
 
     @property
-    def temperature(self):
-        TemperatureReader = FileNameReader("temperature", "K_", "_", indicatorAtStart=False)
+    def temperature(self) -> str:
+        TemperatureReader: FileNameReader = FileNameReader("temperature", "K_", "_", indicatorAtStart=False)
         return TemperatureReader(self.fileName)
 
 if __name__ == "__main__":
