f564008d9be72bdeafbb48466c09bfc73a83b2b7
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-07-27 14:36:31 +0200

    Update docstring

--- pysrc/data_tools/__init__.py
+++ pysrc/data_tools/__init__.py
@@ -1,3 +1,4 @@
 """Contains the file data_formats, which contains different classes that load the measurement data based on their format.
 sort_data sorts the files of the data directory by diameter and by used grating (fine, non-fine) into the sorted_data directory.
+Finally, filename_analysis contains a class that can be used to extract information from filenames.
 """
