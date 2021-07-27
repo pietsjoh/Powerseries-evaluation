ae38714aa71c3387a15e20868d71d34a915438c9
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-07-27 15:13:01 +0200

    Add docstrings

--- pysrc/data_tools/sort_data.py
+++ pysrc/data_tools/sort_data.py
@@ -1,3 +1,5 @@
+"""Contains the class SortData, which can be used to sort measurement data.
+"""
 import sys
 from pathlib import Path
 import shutil
@@ -17,18 +19,72 @@ logger = loggerObj.init_logger(__name__)
 number = typing.Union[float, int]
 floatOrNone = typing.Union[float, None]
 class SortData:
-    _dataModelList: dict = {"QLAB2" : DataQlab2}
-    dataDirPath: Path = (headDirPath / "data").resolve()
+    """Sorts the data files in the data directory.
+
+    The data can be sorted by an attribute in the filename (for example: diameter, temperature, ...).
+    Moreover, the class can sort the data based on the wavelength range (full/fine).
+    So one can easily differentiate between different gratings which are used.
+    Currently, one can only differentiate between 2 different gratings.
+
+    Attributes
+    ----------
+    _dataModelDict: dict, class variable
+        Contains the available data models (currently only Qlab2)
+
+    _dataDirPath: pathlib.Path, class variable
+        path to the data directory
+
+    originalPathsGen: Generator, set by __init__
+        Generator of all *AllSpectra.dat files in the data directory.
+
+    distinguishFullFineSpectra: bool, set by read_data_format_ini_file
+        when set to True, then the files will be sorted based on their wavelength range
+        into fine_spectra/full_spectra folders
+
+    borderFullFineWavelength: float, set by read_data_format_ini_file
+        border wavelength between full and fine spectra in nm.
+        If the wavelength range of the file is larger than this border then it is sorted into the full_spectra folder
+
+    useAttribute: bool, set by read_data_format_ini_file
+        if True then sorting by attribute is enabled
+
+    attrName: str, set by read_data_format_ini_file
+        name of the attribute after that shall be sorted, used for naming the top level directory
+
+    sortedDataDirPath: str, set by read_data_format_ini_file
+        top level directory path to the sorted data
+
+    possibleAttrList: List/None, set by read_data_format_ini_file
+        List of possible values for the attribute
+        If set to None, then any value is accepted
+
+    AttrReader: data_tools.filename_analysis.FileNameReader, set by read_data_format_ini_file
+        FileNameReader object that is used to extract the information about the attribute from the filenames
+
+    Raises
+    ------
+    NotImplementedError:
+        When the datamodel is not QLAB2
+    """
+    _dataModelDict: dict = {"QLAB2" : DataQlab2}
+    _dataDirPath: Path = (headDirPath / "data").resolve()
 
     def __init__(self) -> None:
         logger.debug("Initializing SortData object.")
         self.read_data_format_ini_file()
         if self.dataModel.name == "QLAB2":
-            self.originalPathsGen: typing.Generator[Path, None, None] = self.dataDirPath.rglob("*AllSpectra.dat")
+            self.originalPathsGen: typing.Generator[Path, None, None] = self._dataDirPath.rglob("*AllSpectra.dat")
         else:
             raise NotImplementedError
 
     def read_data_format_ini_file(self) -> None:
+        """Reads the config/data_format.ini file and saves the information in the form of attributes.
+
+        Raises
+        ------
+        TypeError:
+            invalid input for datamodel
+        """
         logger.debug("Calling read_data_format_ini_file()")
         configIniPath: Path = (headDirPath / "config" / "data_format.ini").resolve()
         config: ConfigParser = ConfigParser()
@@ -69,18 +125,30 @@ class SortData:
 
     @property
     def dataModel(self):
+        """The data format that shall be used for loading the data.
+
+        In this class this is only used to extract the wavelength range from the measurement data.
+        When setting this attribute, it is checked whether the value is a key in _dataModelDict.
+        """
         return self._dataModel
 
     @dataModel.setter
     def dataModel(self, value: str):
         logger.debug(f"Setting dataModel to {value}.")
-        if not value.upper() in self._dataModelList.keys():
+        if not value.upper() in self._dataModelDict.keys():
             logger.error(f"{value} is not a valid data model (subclass of DataSuper). Aborting.")
             raise AssertionError("Invalid data model.")
         else:
-            self._dataModel = self._dataModelList[value.upper()]
+            self._dataModel = self._dataModelDict[value.upper()]
 
     def sort_data_attribute(self) -> None:
+        """Sorts the data according to an attribute.
+
+        Raises
+        ------
+        AssertionError:
+            when self.useAttribute is not True
+        """
         logger.debug("Calling sort_data_attribute()")
         assert self.useAttribute
         if not self.sortedDataDirPath.exists():
@@ -127,6 +195,19 @@ class SortData:
                 shutil.copy2(filePath, newFilePath)
 
     def sort_data_fine(self, dirPath: Path) -> None:
+        """Sorts the files inside dirPath into full / fine directories.
+
+        Parameters
+        ----------
+        dirPath: pathlib.Path
+            Path to the directory which shall be sorted.
+
+        Raises
+        ------
+        AssertionError:
+            when dirPath does not exist or is of invalid datatype
+            when self.distinguishFullFineSpectra is not True
+        """
         logger.debug("Calling sort_data_fine")
         assert self.distinguishFullFineSpectra
         assert isinstance(dirPath, Path)
@@ -151,6 +232,10 @@ class SortData:
                 shutil.move(filePath, newFilePath)
 
     def run(self) -> None:
+        """Main method, combines sort_data_attribute() and sort_data_fine().
+
+        This method covers every combination (whether to sort by attribute or full/fine).
+        """
         if self.useAttribute:
             self.sort_data_attribute()
             if self.distinguishFullFineSpectra:
@@ -178,6 +263,8 @@ class SortData:
                 exit()
 
 def main():
+    """Uses the SortData class to sort the data. Is called upon running the script.
+    """
     runSortData: SortData = SortData()
     runSortData.run()
 
