1aa41ee47143e836660ab48c21682532547d4150
Johannes Pietsch <pietsjoh@physik.hu-berlin.de>
2021-08-04 12:20:08 +0200

    Calculate beta for different fixed xi
    Reads the outut file beta_fit.csv to get information about xi.
    The script calculates beta for the estimated fixed xi.
    Here the extracted Q-factor is used.
    
    Moreover, beta is calculated for different xi to sample a distribution.
    This is used to quantify the effet of a change in xi.
    Therefore, the fitting is performed several times
    for equally distributed fixed xi values in the range [xiMin, xiMax].
    xiMin and xiMax are read from beta_fit.csv.
    
    What still needs to be done:
    - think about the final uncertainty of beta (std?)
    - think about xiMin, xiMax values
    - use also fixed original xi as fit?

--- pysrc/output_scripts/betaFitFixedXi.py
+++ pysrc/output_scripts/betaFitFixedXi.py
@@ -1,67 +1,115 @@
-
+## todo: plot distribution, think about final uncertainty of beta,
 def main():
     import numpy as np
-    import pandas as pd
+    import pandas as pd # type:ignore
     import matplotlib.pyplot as plt
-    from scipy.optimize import curve_fit
+    import scipy.optimize as optimize # type:ignore
     from pathlib import Path
+    from functools import partial
+    import typing
     np.set_printoptions(suppress=True)
-    headDir = Path(__file__).resolve().parent
-    filePath = (headDir / "output" / "powerseries.csv").resolve()
+    headDir: Path = Path(__file__).parents[2]
+    filePathPowerseries: Path = (headDir / "output" / "powerseries.csv").resolve()
+    filePathBetaFit: Path = (headDir / "output" / "beta_fit.csv").resolve()
 
-    df = pd.read_csv(filePath, sep="\t")
-    inP = df["in"].to_numpy()
-    outP = df["out"].to_numpy()
-    unc = df["uncOut"].to_numpy()
+    number = typing.Union[int, float, np.number]
 
-    def in_out_curve_with_xi(x, beta, p, a, xi):
+    ## loading the powerseries data
+    dfPowerseries: pd.DataFrame = pd.read_csv(filePathPowerseries, sep="\t")
+    inP: np.ndarray = dfPowerseries["in"].to_numpy()
+    outP: np.ndarray = dfPowerseries["out"].to_numpy()
+    uncOutP: np.ndarray = dfPowerseries["uncOut"].to_numpy()
+
+    ## loading the data from the beta fit, including the values for xi
+    dfBetaFit: pd.DataFrame = pd.read_csv(filePathBetaFit, sep="\t")
+    xiMin: number = dfBetaFit["xiMin"].to_numpy()[0]
+    xiMax: number = dfBetaFit["xiMax"].to_numpy()[0]
+    xiMiddle: number = (xiMax - xiMin) / 2
+    assert xiMiddle > 0
+    xiEstimateFit: number = dfBetaFit["xiEstimateFit"].to_numpy()[0]
+    fitParamsWithXi: np.ndarray = dfBetaFit["fitParamsBeta"].to_numpy()
+    uncFitParamsWithXi: np.ndarray = dfBetaFit["uncFitParamsBeta"].to_numpy()
+
+    ## defining the output curve, including xi as a parameter
+    def in_out_curve(x, beta, p, a, xi):
         return a / beta * ( p*x / (p*x + 1) * (1 + xi*beta) * (1 + beta*p*x) - (beta**2)*xi*p*x)
 
-    xiEstimate = 23.70134
+    def log_in_out_curve(x, beta, p, a, xi):
+        return np.log(in_out_curve(x, beta, p, a, xi))
 
-    in_out_curve_without_xi = lambda x, beta, p, a : in_out_curve_with_xi(x, beta, p, a, xi=xiEstimate)
-
-    def log_in_out_curve_with_xi(x, *args):
-        return np.log(in_out_curve_with_xi(x, *args))
-
-    def log_in_out_curve_without_xi(x, *args):
-        return np.log(in_out_curve_without_xi(x, *args))
-
+    ## definitions of initial parameter guesses and boundaries for the fit parameters
     boundsWithXi = (0, [1, np.inf, np.inf, np.inf])
     boundsWithoutXi = (0, [1, np.inf, np.inf])
-    p0WithXi = (0.05, 1, 1, xiEstimate)
-    p0WithoutXi = (0.05, 1, 1)
-    weights = unc
+    p0WithXi = (0.5, 1, 1, xiMiddle)
+    p0WithoutXi = (0.5, 1, 1)
+    weights = uncOutP
 
+    ## fitting using the estimated xi from the fit (using the Q-factor from the fit)
+    log_in_out_curve_xi_estimate_fit = partial(log_in_out_curve, xi=xiEstimateFit)
+    pXiEstimateFit, covXiEstimateFit = optimize.curve_fit(log_in_out_curve_xi_estimate_fit, outP, np.log(inP),
+        p0=p0WithoutXi, bounds=boundsWithoutXi)
 
-    pWithXi, covWithXi = curve_fit(log_in_out_curve_with_xi, outP, np.log(inP), p0=p0WithXi)
-    pWithoutXi, covWithoutXi = curve_fit(log_in_out_curve_without_xi, outP, np.log(inP), p0=p0WithoutXi)
+    ## fitting using different equally distributed values for in the range [xiMin, xiMax]
+    ## the final value for beta and its uncertainty is obtained by calculating the mean / std of the distribution
+    numSamples: int = 1000
+    xiArr: np.ndarray = np.linspace(xiMin, xiMax, numSamples)
+    betaArr: np.ndarray = np.empty(numSamples)
+    uncBetaArr: np.ndarray = np.empty(numSamples)
+    for i, xi in enumerate(xiArr):
+        log_in_out_curve_fixed_xi = partial(log_in_out_curve, xi=xi)
+        try:
+            p, cov = optimize.curve_fit(log_in_out_curve_fixed_xi, outP, np.log(inP),
+                p0=p0WithoutXi, bounds=boundsWithoutXi)
+        except RuntimeError:
+            print(f"fitting did not work for xi={xi}.")
+            betaArr[i] = np.nan
+            uncBetaArr[i] = np.nan
+        else:
+            unc: np.ndarray = np.sqrt(np.diag(cov))
+            betaArr[i] = p[0]
+            uncBetaArr[i] = unc[0]
+
+    meanBeta: number = np.nanmean(betaArr)
+    meanUncBeta: number = np.nanmean(uncBetaArr)
+    stdBeta: number = np.nanstd(betaArr)
 
     print("fit with xi as parameter:")
-    print(f"parameters: {pWithXi}")
-    print(f"uncertainty: {np.sqrt(np.diag(covWithXi))}")
+    print(f"parameters:                     {fitParamsWithXi}")
+    print(f"uncertainty:                    {uncFitParamsWithXi}")
+    print(f"beta factor:                    {fitParamsWithXi[0]:.7f} \u00B1 {uncFitParamsWithXi[0]:.7f}")
+    print()
     print("-"*100)
-    print("fit with fixed xi")
-    print(f"parameters: {pWithoutXi}")
-    print(f"uncertainty: {np.sqrt(np.diag(covWithoutXi))}")
-    print(f"beta factor fixed xi: {pWithoutXi[0]:.7f} \u00B1 {np.sqrt(np.diag(covWithoutXi))[0]:.7f}")
+    print()
+    print("fit with fixed xi, using the estimated xi (with Q-factor)")
+    print(f"parameters:                     {pXiEstimateFit}")
+    print(f"uncertainty:                    {np.sqrt(np.diag(covXiEstimateFit))}")
+    print(f"beta factor:                    {pXiEstimateFit[0]:.7f} \u00B1 {np.sqrt(np.diag(covXiEstimateFit))[0]:.7f}")
+    print()
+    print("-"*100)
+    print()
+    print("fit using multiple values for a fixed xi to sample a distribution")
+    print(f"range for xi:                   [{xiMin, xiMax}]")
+    print(f"mean beta factor:               {meanBeta}")
+    print(f"mean beta factor uncertainty:   {meanUncBeta}")
+    print(f"std beta factor:                {stdBeta}")
 
-    fig, axs = plt.subplots(nrows=1, ncols=2)
-    ax1, ax2 = axs
-    outputPlotArr = np.logspace(np.log10(np.amin(outP)), np.log10(np.amax(outP)), 100)
-    inputPlotArrWithXi = in_out_curve_with_xi(outputPlotArr, *pWithXi)
-    inputPlotArrWithoutXi = in_out_curve_without_xi(outputPlotArr, *pWithoutXi)
-    ax1.errorbar(inP, outP, yerr=unc, fmt="b.", capsize=3)
-    ax2.errorbar(inP, outP, yerr=unc, fmt="b.", capsize=3)
-    ax1.plot(inputPlotArrWithXi, outputPlotArr, color="orange")
-    ax1.set_title("fit with xi as parameter")
-    ax1.set_yscale("log")
-    ax1.set_xscale("log")
-    ax2.plot(inputPlotArrWithoutXi, outputPlotArr, color="orange")
-    ax2.set_title("fit with fixed xi")
-    ax2.set_xscale("log")
-    ax2.set_yscale("log")
-    plt.show()
+
+    # fig, axs = plt.subplots(nrows=1, ncols=2)
+    # ax1, ax2 = axs
+    # outputPlotArr = np.logspace(np.log10(np.amin(outP)), np.log10(np.amax(outP)), 100)
+    # inputPlotArrWithXi = in_out_curve_with_xi(outputPlotArr, *pWithXi)
+    # inputPlotArrWithoutXi = in_out_curve_without_xi(outputPlotArr, *pWithoutXi)
+    # ax1.errorbar(inP, outP, yerr=unc, fmt="b.", capsize=3)
+    # ax2.errorbar(inP, outP, yerr=unc, fmt="b.", capsize=3)
+    # ax1.plot(inputPlotArrWithXi, outputPlotArr, color="orange")
+    # ax1.set_title("fit with xi as parameter")
+    # ax1.set_yscale("log")
+    # ax1.set_xscale("log")
+    # ax2.plot(inputPlotArrWithoutXi, outputPlotArr, color="orange")
+    # ax2.set_title("fit with fixed xi")
+    # ax2.set_xscale("log")
+    # ax2.set_yscale("log")
+    # plt.show()
 
 if __name__ == "__main__":
     main()
